714d12ae339dce029752746e94f41f47583b29ce
Liraz Siri <liraz@turnkeylinux.org>
2010-08-23 09:02:41 +0300

    moves some generic functions to utils.py module

--- pylib/deck.py
+++ pylib/deck.py
@@ -10,34 +10,7 @@ from paths import Paths
 
 import aufs
 
-def make_relative(root, path):
-    """Return <path> relative to <root>.
-
-    For example:
-        make_relative("../../", "file") == "path/to/file"
-        make_relative("/root", "/tmp") == "../tmp"
-        make_relative("/root", "/root/backups/file") == "backups/file"
-        
-    """
-
-    up_count = 0
-
-    root = realpath(root).rstrip('/')
-    path = realpath(path).rstrip('/')
-
-    while True:
-        if path == root or path.startswith(root.rstrip("/") + "/"):
-            return ("../" * up_count) + path[len(root) + 1:]
-
-        root = dirname(root).rstrip('/')
-        up_count += 1
-
-def makedirs(path):
-    try:
-        os.makedirs(path)
-    except OSError, e:
-        if e[0] != errno.EEXIST:
-            raise
+from utils import makedirs, make_relative
 
 class Error(Exception):
     pass
